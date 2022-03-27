2b1dbfb19d2f2855a60f08dfc78aa79ddfb1af1b
Daniel Milligan <ddmilligan@cpp.edu>
2022-03-27 13:20:10 -0700

    Second iteration of Requests - Adds readChar and readPositiveInteger - Adds some (not all) menu option requests - Adds requestPurpose to phone number and email requests

--- include/Requests.h
+++ include/Requests.h
@@ -1,14 +1,33 @@
-// WARNING - ALL CHAR ARRAYS RETURNED BY THESE METHODS ARE
-// DYNAMICALLY ALLOCATED. TO PREVENT MEMORY LEAKS, MAKE SURE
-// YOU PROPERLY DEALLOCATE THEM AFTER YOU'RE DOING USING THEM.
+#include "address_book.h"
+
+/*
+WARNING - ALL CHAR ARRAYS RETURNED BY THE FOLLOWING THREE 
+METHODS ARE DYNAMICALLY ALLOCATED. TO PREVENT MEMORY LEAKS, 
+MAKE SURE YOU PROPERLY DEALLOCATE THEM AFTER YOU'RE DONE USING THEM.
+*/
 
 // Request and validate a user-inputted name.
 char *requestNameInput();
 
 // Request and validate a user-inputted phone number
 // phoneNumberIndex - An integer between 1 and 5, inclusive
-char *requestPhoneNumberInput(int phoneNumberIndex);
+// requestPurpose - In what sub-menu is the request being made?
+char *requestPhoneNumberInput(int phoneNumberIndex, Modes requestPurpose);
 
 // Requests and validate a user-inputted email address
-// emailAddressIndex - An integer betweeen 1 and 5, inclusive/
-char *requestEmailAddressInput(int emailAddressIndex);
+// emailAddressIndex - An integer betweeen 1 and 5, inclusive
+// requestPurpose - In what sub-menu is the request being made?
+char *requestEmailAddressInput(int emailAddressIndex, Modes requestPurpose);
+
+/*
+The values returned by the following functions are NOT
+dynamically allocated.
+*/
+
+// Use to request the user input a number in the Main Menu.
+// Returns a MenuFeatures enum (an integer between 0 and 6)
+MenuFeatures requestMainMenuInput();
+
+// Use to request the user input a number in the Add Contact Menu.
+// Returns a MenuOptions enum (a value between 0 and 3)
+MenuOptions requestAddContactMenuInput();
--- src/Requests.c
+++ src/Requests.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -180,25 +181,41 @@ char *requestNameInput()
     return readString(inputPrompt, errorPrompt, NAME_LEN, readName);
 }
 
-char *requestPhoneNumberInput(int phoneNumberIndex)
+char *requestPhoneNumberInput(int phoneNumberIndex, Modes requestPurpose)
 {
-    char inputPromptBuffer[30];
-    sprintf(inputPromptBuffer, "%s%d%s", "Enter Phone Number ", phoneNumberIndex, ": ");
-
+    char inputPromptBuffer[300];
     char *inputPrompt = inputPromptBuffer;
     char *errorPrompt = "ERROR - Improper Input - Phone Numbers Must:\n"
         "> Range from 0-32 characters [Inputting nothing cancels your choice].\n"
         "> Consist only of numbers.\n"
         "Please try again.\n";
+
+    switch(requestPurpose)
+    {
+        case e_add:
+            goto ADD_CONTACT;
+
+        case e_search:
+
+        case e_edit:
+
+        case e_delete:
+
+        case e_list:
+        break;
+    }
+
+    ADD_CONTACT:
+    sprintf(inputPromptBuffer, "%s%d%s", "Enter Phone Number ", phoneNumberIndex, 
+        ": [You may enter up to 5 Phone Numbers by selecting the same option after this]: ");
     
+    inputPrompt = inputPromptBuffer;
     return readString(inputPrompt, errorPrompt, NUMBER_LEN, readPhoneNumber);
 }
 
-char *requestEmailAddressInput(int emailAddressIndex)
+char *requestEmailAddressInput(int emailAddressIndex, Modes requestPurpose)
 {
-    char inputPromptBuffer[30];
-    sprintf(inputPromptBuffer, "%s%d%s", "Enter Email ID ", emailAddressIndex, ": ");
-
+    char inputPromptBuffer[300];
     char *inputPrompt = inputPromptBuffer;
     char *errorPrompt = "ERROR - Improper Input - Email Addresses Must:\n"
         "> Range from 0-32 characters [Inputting nothing cancels your choice].\n"
@@ -207,9 +224,122 @@ char *requestEmailAddressInput(int emailAddressIndex)
         "> The usage of certain special characters may be restricted.\n"
         "Please try again.\n";
 
-    readString(inputPrompt, errorPrompt, EMAIL_ID_LEN, readEmailAddress);
+    switch(requestPurpose)
+    {
+        case e_add:
+            goto ADD_CONTACT;
+
+        case e_search:
+
+        case e_edit:
+
+        case e_delete:
+
+        case e_list:
+        break;
+    }
+
+    // Add contact request
+    ADD_CONTACT:
+    sprintf(inputPromptBuffer, "%s%d%s", "Enter Email ID ", emailAddressIndex, 
+        ": [You may enter up to 5 Email IDs by selecting the same option after this]: ");
+    inputPrompt = inputPromptBuffer;
+    return readString(inputPrompt, errorPrompt, EMAIL_ID_LEN, readEmailAddress);
 }
 
+char readChar(char *inputPrompt, char *errorPrompt, char *validInputs, int numberValidInputs)
+{
+    char inputChar;
+    int i = 0;
+
+    askForInput:
+    printf("%s", inputPrompt);
+
+    inputChar = getchar();
+    if(inputChar == EOF || inputChar == '\n')
+        goto singleCharInput;
+    else
+    {
+        if(flushBuffer() > 0)
+            goto badInput;
+    }
+
+    singleCharInput:
+    for(i = 0; i < numberValidInputs; i++)
+    {
+        if(validInputs[i] == inputChar)
+            return inputChar;
+    }
+
+    badInput:
+    printf("%s", errorPrompt);
+    goto askForInput;
+}
+
+int readPositiveInteger(char *inputPrompt, char *errorPrompt, int minimumValue, int maximumValue)
+{
+    const int MAX_NUMBER_DIGITS = 9;
+    char number[MAX_NUMBER_DIGITS];
+    char currentChar;
+    int i = 0, j = 0, value = 0;
+
+    askForInput:
+    printf("%s", inputPrompt);
+
+    for(i = 0, value = 0; i < MAX_NUMBER_DIGITS; i++)
+    {
+        currentChar = getchar();
+        switch(currentChar)
+        {
+            case EOF: case '\n':
+                if(i == 0)
+                    goto badInput;
+                else
+                    goto goodInput;
+            
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+                goto goodCharInput;
+            
+            default:
+                flushBuffer();
+                goto badInput;
+        }
+        goodCharInput:
+        number[i] = currentChar;
+    }
+    if(flushBuffer() > 0)
+        goto badInput;
+
+    goodInput:
+    for(j = 0; j < i; j++)
+        value += pow(10, i - j - 1) * (number[j] - '0');
+
+    if(value < minimumValue || value > maximumValue)
+        goto badInput;
+    else
+        return value;
+
+    badInput:
+    printf("%s", errorPrompt);
+    goto askForInput;
+}
+
+MenuFeatures requestMainMenuInput()
+{
+    char *inputPrompt = "Please select an option: ";
+    char *errorPrompt = "ERROR - Please enter a value between 0 and 6\n";
+
+    return readPositiveInteger(inputPrompt, errorPrompt, 0, 6);
+}
+
+MenuOptions requestAddContactMenuInput()
+{
+    char *inputPrompt = "Please select an option: ";
+    char *errorPrompt = "ERROR - Please enter a value between 0 and 3\n";
+
+    return readPositiveInteger(inputPrompt, errorPrompt, 0, 3);
+}
 /*
 int main()
 {
@@ -219,13 +349,17 @@ int main()
         printf("%s\n", name);
         free(name);
 
-        char * number = requestPhoneNumberInput(1);
+        
+        char * number = requestPhoneNumberInput(1, e_add);
         printf("%s\n", number);
         free(number);
 
-        char * email = requestEmailAddressInput(1);
+        char * email = requestEmailAddressInput(1, e_add);
         printf("%s\n", email);
         free(email);
+
+        printf("%d\n", requestMainMenuInput());
+        printf("%d\n", requestAddContactMenuInput());
     }
     return 0;
 }
